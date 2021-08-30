2285450add4706fce727692883b29fa5c5382612
Aditya Lucky Septiadi <54019824+adityalstkp@users.noreply.github.com>
2021-08-30 20:03:40 +0700

    improvement: with template (#2)

--- backend/scripts/run.sh
+++ backend/scripts/run.sh
@@ -8,6 +8,17 @@ if [ "$WAIT_STATUS" -ne 0 ]; then
   exit "$WAIT_STATUS"
 fi
 
+NAME="play-backend"
+PORT=3000
+
+# add service json consul agent
+mkdir -p /etc/consul/consul.d/
+/scripts/consul.service.node.template.sh $NAME $PORT > /etc/consul/consul.d/service-backend.json
+
+# add consul-agent config
+mkdir -p /var/consul/config/
+/scripts/consul.agent.template.sh $NAME > /var/consul/config/config.json
+
 consul agent -config-file=/var/consul/config/config.json -config-dir=/etc/consul/consul.d -join=consul.play.local -retry-join "consul.play.local" &
 
 # Run the main container command
---
+++ scripts/consul.service.node.template.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+NAME=$1
+PORT=$2
+ADDR=$(hostname)
+
+cat  << EOF
+{
+    "service": {
+        "name": "$NAME",
+        "port": $PORT,
+        "address": "$ADDR",
+        "checks": [
+            {
+                "interval": "5s",
+                "name": "nodejs checks",
+                "tcp": "127.0.0.1:$PORT"
+            }
+        ],
+        "tags": []
+    }
+}
+EOF
